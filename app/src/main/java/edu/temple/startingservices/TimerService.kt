373573430ac34578e659c8e455020b0d08f1f3c2
Eric Reizas <tuo17224@temple.edu>
2024-04-17 15:45:06 -0400

    Implemented runTimer function

--- app/src/main/java/edu/temple/startingservices/TimerService.kt
+++ app/src/main/java/edu/temple/startingservices/TimerService.kt
@@ -3,10 +3,29 @@ package edu.temple.startingservices
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class TimerService : Service() {
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        runTimer()
+        return super.onStartCommand(intent, flags, startId)
+    }
+    fun runTimer(time : Int){
+        CoroutineScope(Job() + Dispatchers.Default).launch{
+            repeat(time){
+                Log.d("Countdown","${time-it}")
+                delay(1000)
+            }
+        }
     }
 }
